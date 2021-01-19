e44d8397e8c7d8adbee1dda62201fbf4482143c9
balboa342 <42502796+balboa342@users.noreply.github.com>
2021-01-19 11:08:42 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function regSW(){
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js');
+}
+
+document.querySelector('#show').addEventListener('click', () => {
+  const iconUrl = document.querySelector('select').selectedOptions[0].value;
+  let imgElement = document.createElement('img');
+  imgElement.src = iconUrl;
+  document.querySelector('#container').appendChild(imgElement);
+});
+}
+
+/*
+window.onload = () => {
+	'use strict';
+	
+	if('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('./sw.js');
+	}
+}
+*/
