3636136879d87da87a589c07225aded4c96e8e8e
balboa342 <42502796+balboa342@users.noreply.github.com>
2021-01-19 15:11:53 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const birds = [
   { name: "California Gull", image: "images/CaliGull.png" },
   { name: "Cardinal", image: "images/Cardinal.png" },
   { name: "Chicken", image: "images/Chicken.png" },
-  { name: "Meadowlard", image: "images/Meadowlard.png" },
+  { name: "Meadowlard", image: "images/Meadowlark.png" },
   { name: "Mockingbird", image: "images/Mockingbird.png" }
 ]
 
@@ -15,7 +15,7 @@ const showBirds = () => {
               <div class="card">
                 <img class="card--avatar" src=${image} />
                 <h1 class="card--title">${name}</h1>
-                <a class="card--link" href="#">Taste</a>
+                <a class="card--link" href="#">Larger</a>
               </div>
               `)
   )
