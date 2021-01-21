215c28c4a03876dfcc3cfa93ac465263d7738790
balboa342 <42502796+balboa342@users.noreply.github.com>
2021-01-21 12:55:52 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,24 +4,24 @@ const RUNTIME = 'runtime';
 const staticStateBirds = "state-birds-site-v1"
 // A list of local resources we always want to be cached.
 const assets = [
-  'index.html',
+  '/index.html',
   './', // Alias for index.html
-  'css/style.css',
-  'js/app.js',
-  'images/hello-icon-128.png',
-  'images/hello-icon-144.png',
-  'images/hello-icon-152.png',
-  'images/hello-icon-192.png',
-  'images/hello-icon-196maskable.png',
-  'images/hello-icon-256.png',
-  'images/hello-icon-512.png',
-  'images/Cardinal.png',
-  'images/CaliGuyll.png',
-  'images/Chicken.png',
-  'images/Meadowlark.png',
-  'images/Mockingbird.png',
-  'favicon.ico',
-  'sw.js'
+  '/css/style.css',
+  '/js/app.js',
+  '/images/hello-icon-128.png',
+  '/images/hello-icon-144.png',
+  '/images/hello-icon-152.png',
+  '/images/hello-icon-192.png',
+  '/images/hello-icon-196maskable.png',
+  '/images/hello-icon-256.png',
+  '/images/hello-icon-512.png',
+  '/images/Cardinal.png',
+  '/images/CaliGuyll.png',
+  '/images/Chicken.png',
+  '/images/Meadowlark.png',
+  '/images/Mockingbird.png',
+  '/favicon.ico',
+  '/sw.js'
 ];
 
 self.addEventListener("install", installEvent => {
