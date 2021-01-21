e60e2aabffb0276e2710508df18fa8d479530ab9
balboa342 <42502796+balboa342@users.noreply.github.com>
2021-01-21 12:36:44 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const RUNTIME = 'runtime';
 const staticStateBirds = "state-birds-site-v1"
 // A list of local resources we always want to be cached.
 const assets = [
-  'index.html',
+  'PWA01/index.html',
   './', // Alias for index.html
   'css/style.css',
   'js/app.js',
