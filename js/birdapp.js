42150606d091a6e7f66f691f0e073cf0faadcf68
balboa342 <42502796+balboa342@users.noreply.github.com>
2021-01-21 14:05:56 -0600

    Create birdapp.js

---
+++ js/birdapp.js
@@ -0,0 +1,24 @@
+const container = document.querySelector(".container")
+const birds = [
+  { name: "Cardinal", image: "images/Cardinal.png" },
+  { name: "Chicken", image: "images/Chicken.png" },
+  { name: "California Gull", image: "images/CaliGull.png" },
+  { name: "Meadowlark", image: "images/Meadowlark.png" },
+  { name: "Mockingbird", image: "images/Mockingbird.png" }
+]
+
+const showBirds = () => {
+  let output = ""
+  birds.forEach(
+    ({ name, image }) =>
+      (output += `
+              <div class="card">
+                <img class="card--avatar" src=${image} />
+                <h1 class="card--title">${name}</h1>
+                <a class="card--link" href="#">Taste</a>
+              </div>
+              `)
+  )
+  container.innerHTML = output
+  
+document.addEventListener("DOMContentLoaded", showBirds)
